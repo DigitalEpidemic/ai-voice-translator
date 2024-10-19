2c09b649e21074c29bff52e5546a1a19737d1127
Jeffrey Polasz <jeff_polasz@hotmail.com>
2024-10-19 02:41:51 -0400

    Add audio player for uploaded file

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 function App(): JSX.Element {
+  const [audioUrl, setAudioUrl] = useState<string | null>(null)
+
   const sendFileToService = async (event: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
     if (!event.target.files) {
       return
@@ -7,13 +11,26 @@ function App(): JSX.Element {
     const audioFileArrayBuffer = await audioFile.arrayBuffer()
     const byteArray = new Uint8Array(audioFileArrayBuffer)
 
+    const fileBlob = new Blob([byteArray], { type: 'audio/wav' })
+    const blobUrl = URL.createObjectURL(fileBlob)
+    setAudioUrl(blobUrl)
+
     await window.api.voiceFileUpload(byteArray) // TODO: Change to two-way communication and return transcribed text
   }
 
   return (
     <>
       <label htmlFor="voice-upload">Upload Voice File</label>
-      <input id="voice-upload" type="file" onChange={sendFileToService} />
+      <input id="voice-upload" type="file" accept="audio/*" onChange={sendFileToService} />
+
+      {audioUrl && (
+        <div>
+          <audio controls>
+            <source src={audioUrl} type="audio/wav" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+      )}
     </>
   )
 }
