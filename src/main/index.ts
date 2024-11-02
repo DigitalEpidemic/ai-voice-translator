b3786c18ec98c998db224840a1046da0eb08c800
Jeffrey Polasz <jeff_polasz@hotmail.com>
2024-11-02 05:32:03 -0400

    Increase app height

--- src/main/index.ts
+++ src/main/index.ts
@@ -24,7 +24,7 @@ function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
-    height: 670,
+    height: 750,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
