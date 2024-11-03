fbfa97e9ed89561692caf5fb9d44a22db1cf218f
Jeffrey Polasz <jeff_polasz@hotmail.com>
2024-11-03 00:07:17 -0400

    Add basic styling to app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,19 @@
 import { AvailableLanguageCodes, languages } from '@/types/languageTypes'
-import { Button, Heading, HStack, Select, Stack, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Select,
+  Stack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text
+} from '@chakra-ui/react'
 import React, { useState } from 'react'
 import WavEncoder from 'wav-encoder'
 import { FileUploadButton } from './components/ui/FileUpload'
@@ -15,6 +29,7 @@ const App = (): React.ReactElement => {
   const [transcription, setTranscription] = useState<string>('')
   const [translatedText, setTranslatedText] = useState<string>('')
   const [outputLanguage, setOutputLanguage] = useState<AvailableLanguageCodes>(languages[0].code)
+  const [tabIndex, setTabIndex] = useState(0)
 
   const handleAudioFileUpload = async (
     event: React.ChangeEvent<HTMLInputElement>
@@ -137,34 +152,61 @@ const App = (): React.ReactElement => {
   }
 
   return (
-    <Stack w={'md'}>
+    <Tabs onChange={(index) => setTabIndex(index)} isFitted variant="enclosed-colored" w={'100vw'}>
       <Heading size="lg" my={2} textAlign={'center'}>
         AI Voice Translator
       </Heading>
-      <Text>Upload Existing Audio File</Text>
-      <FileUploadButton buttonText="Upload File" onChange={handleAudioFileUpload} />
+      <TabList mb="1em">
+        <Tab>Speech-To-Speech</Tab>
+        <Tab>Translate File</Tab>
+        <Tab>Text-to-Speech</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Stack maxW={'500px'} mx="auto">
+            <Text>Record Microphone:</Text>
+            <HStack w={'100%'} justifyContent={'center'}>
+              <Button
+                flexGrow={1}
+                onClick={handleStartRecording}
+                isDisabled={isStartButtonDisabled}
+              >
+                Start Recording
+              </Button>
+              <Button flexGrow={1} onClick={handleStopRecording} isDisabled={isStopButtonDisabled}>
+                Stop Recording
+              </Button>
+            </HStack>
+          </Stack>
+        </TabPanel>
+        <TabPanel>
+          <Stack maxW={'500px'} mx="auto">
+            <Text>Upload Existing Audio File:</Text>
+            <FileUploadButton buttonText="Upload File" onChange={handleAudioFileUpload} />
+          </Stack>
+        </TabPanel>
+        <TabPanel>
+          <Stack maxW={'500px'} mx="auto">
+            <Text>Not yet implemented 😢</Text>
+          </Stack>
+        </TabPanel>
+      </TabPanels>
 
-      <Text textAlign={'center'}>Or</Text>
+      <Stack maxW={'500px'} mx="auto">
+        {tabIndex !== 2 && (
+          <>
+            <Box mb={4}>
+              <audio controls style={{ width: '100%' }}>
+                {originalAudioUrl && <source src={originalAudioUrl} type="audio/wav" />}
+                Your browser does not support the audio element.
+              </audio>
+            </Box>
+            <Button onClick={transcribeAudioInArrayBuffer}>Transcribe</Button>
+            <Text>{transcription}</Text>
+          </>
+        )}
 
-      <Text>Record Microphone</Text>
-      <HStack>
-        <Button onClick={handleStartRecording} isDisabled={isStartButtonDisabled}>
-          Start Recording
-        </Button>
-        <Button onClick={handleStopRecording} isDisabled={isStopButtonDisabled}>
-          Stop Recording
-        </Button>
-      </HStack>
-
-      {originalAudioUrl && (
-        <Stack>
-          <audio controls>
-            <source src={originalAudioUrl} type="audio/wav" />
-            Your browser does not support the audio element.
-          </audio>
-          <Button onClick={transcribeAudioInArrayBuffer}>Transcribe</Button>
-          <Text>{transcription}</Text>
-          <Button onClick={handleTranslatingText}>Translate</Button>
+        <Flex>
           <Select
             name="languages"
             id="languages"
@@ -177,18 +219,22 @@ const App = (): React.ReactElement => {
               </option>
             ))}
           </Select>
-          <Text>{translatedText}</Text>
+          <Button w={'100%'} onClick={handleTranslatingText}>
+            Translate
+          </Button>
+        </Flex>
+        <Text>{translatedText}</Text>
 
+        <Stack mt={4}>
           <Button onClick={handleTextToSpeech}>Use AI Voice 🤖</Button>
-          {translatedAudioUrl && (
-            <audio controls>
-              <source src={translatedAudioUrl} type="audio/mp3" />
-              Your browser does not support the audio element.
-            </audio>
-          )}
+          <Text>Translated Audio:</Text>
+          <audio controls style={{ width: '100%' }}>
+            {translatedAudioUrl && <source src={translatedAudioUrl} type="audio/mp3" />}
+            Your browser does not support the audio element.
+          </audio>
         </Stack>
-      )}
-    </Stack>
+      </Stack>
+    </Tabs>
   )
 }
 
